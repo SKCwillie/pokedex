f84207394b972c27458948fc769cea28cb625042
giginet <giginet.net@gmail.com>
2022-07-18 13:49:21 +0900

    Drop 2.7 support

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         ],
     },
     classifiers=[
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.7",
